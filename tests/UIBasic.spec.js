56ffca892b1bd1e8ac6569e1787201d58a37e45e
jvibz <jvibz@jvibz.com>
2026-04-23 13:50:34 +0200

    Showing context for browser and page

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -1,5 +1,12 @@
 const { test, expect } = require('@playwright/test');
 
-test('first playwright test', async ({ browser, page }) => {
+test('Browser context playwright test', async ({ browser }) => {
+  // when you want to give more context to the browser, you can use browser context. It is like incognito mode in the browser. It will not share cookies and cache with other contexts.
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
 
+test('Page context playwright test', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 });
